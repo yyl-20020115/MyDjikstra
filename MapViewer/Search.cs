332330835966a7d155560dc3623c4f34f8f68e61
yyl20050115 <yyl_20050115@hotmail.com>
2023-04-06 07:13:22 +0800

    fx

--- MapViewer/Search.cs
+++ MapViewer/Search.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace MapViewer;
 
 public class SearchEngine
 {
     public event EventHandler Updated;
-    private void OnUpdated() => Updated?.Invoke(null, EventArgs.Empty);
+    //private void OnUpdated() => Updated?.Invoke(null, EventArgs.Empty);
     public readonly Map Map;
     public readonly Node Start;
     public readonly Node End;
@@ -17,30 +18,40 @@ public class SearchEngine
 
     public SearchEngine(Map map)
     {
-        Start = map.StartNode;
-        End = map.EndNode;
-        Map = map;
+        this.Start = map.StartNode;
+        this.End = map.EndNode;
+        this.Map = map;
     }
 
     public List<Node> GetShortestPathDijkstra()
     {
-        DijkstraSearch();
+        this.DijkstraSearch();
         var shortestPath = new List<Node> { End };
-        BuildShortestPath(shortestPath, End);
+        this.BuildShortestPath(shortestPath, End);
+        shortestPath.Reverse();
+        return shortestPath;
+    }
+    public List<Node> GetShortestPathAStart()
+    {
+        foreach (var node in Map.Nodes)
+            node.StraightLineDistanceToEnd = node.GetDistanceTo(End);
+        this.AStarSearch();
+        var shortestPath = new List<Node> { End };
+        this.BuildShortestPath(shortestPath, End);
         shortestPath.Reverse();
         return shortestPath;
     }
 
-    private void BuildShortestPath(List<Node> list, Node node)
+    protected void BuildShortestPath(List<Node> list, Node node)
     {
         if (node.NearestToStart == null) return;
         list.Add(node.NearestToStart);
-        ShortestPathLength += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Length;
-        ShortestPathCost += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Cost;
-        BuildShortestPath(list, node.NearestToStart);
+        this.ShortestPathLength += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Length;
+        this.ShortestPathCost += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Cost;
+        this.BuildShortestPath(list, node.NearestToStart);
     }
 
-    private void DijkstraSearch()
+    protected void DijkstraSearch()
     {
         NodeVisits = 0;
         Start.MinCostToStart = 0;
@@ -49,21 +60,23 @@ public class SearchEngine
         do
         {
             NodeVisits++;
-            priorityQueue = priorityQueue.OrderBy(x => x.MinCostToStart.Value).ToList();
+            priorityQueue = priorityQueue.OrderBy(x => x.MinCostToStart.GetValueOrDefault()).ToList();
             var node = priorityQueue.First();
             priorityQueue.Remove(node);
             foreach (var cnn in node.Connections.OrderBy(x => x.Cost))
             {
                 var childNode = cnn.ConnectedNode;
-                if (childNode.Visited)
-                    continue;
-                if (childNode.MinCostToStart == null ||
-                    node.MinCostToStart + cnn.Cost < childNode.MinCostToStart && node.MinCostToStart + cnn.Cost < startToEndCost)
+                if (!childNode.Visited)
                 {
-                    childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
-                    childNode.NearestToStart = node;
-                    if (!priorityQueue.Contains(childNode))
-                        priorityQueue.Add(childNode);
+                    if (childNode.MinCostToStart == null ||
+                        node.MinCostToStart + cnn.Cost < childNode.MinCostToStart 
+                        && node.MinCostToStart + cnn.Cost < startToEndCost)
+                    {
+                        childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
+                        childNode.NearestToStart = node;
+                        if (!priorityQueue.Contains(childNode))
+                            priorityQueue.Add(childNode);
+                    }
                 }
             }
             node.Visited = true;
@@ -72,18 +85,8 @@ public class SearchEngine
         } while (priorityQueue.Any());
     }
 
-    public List<Node> GetShortestPathAStart()
-    {
-        foreach (var node in Map.Nodes)
-            node.StraightLineDistanceToEnd = node.GetDistanceTo(End);
-        AStarSearch();
-        var shortestPath = new List<Node> { End };
-        BuildShortestPath(shortestPath, End);
-        shortestPath.Reverse();
-        return shortestPath;
-    }
 
-    private void AStarSearch()
+    protected void AStarSearch()
     {
         NodeVisits = 0;
         Start.MinCostToStart = 0;
@@ -93,18 +96,22 @@ public class SearchEngine
             var node = priorityQueue.First();
             priorityQueue.Remove(node);
             NodeVisits++;
+            
+            var bag = new ConcurrentBag<Node>();
+
             foreach (var cnn in node.Connections.OrderBy(x => x.Cost))
             {
                 var childNode = cnn.ConnectedNode;
-                if (childNode.Visited)
-                    continue;
-                if (childNode.MinCostToStart == null ||
-                    node.MinCostToStart + cnn.Cost < childNode.MinCostToStart)
+                if (!childNode.Visited)
                 {
-                    childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
-                    childNode.NearestToStart = node;
-                    if (!priorityQueue.Contains(childNode))
-                        priorityQueue.Add(childNode);
+                    if (childNode.MinCostToStart == null ||
+                        node.MinCostToStart + cnn.Cost < childNode.MinCostToStart)
+                    {
+                        childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
+                        childNode.NearestToStart = node;
+                        if (!priorityQueue.Contains(childNode))
+                            priorityQueue.Add(childNode);
+                    }
                 }
             }
             node.Visited = true;
