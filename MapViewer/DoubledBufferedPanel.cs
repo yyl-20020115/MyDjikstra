184b412ca53245d8107dea11c72be959a10e42f1
yyl20050115 <yyl_20050115@hotmail.com>
2023-04-05 13:51:22 +0800

    添加项目文件。

---
+++ MapViewer/DoubledBufferedPanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapViewer
+{
+    public class DoubledBufferedPanel : Panel
+    {
+        public DoubledBufferedPanel()
+        {
+            base.DoubleBuffered = true;
+        }
+    }
+}
